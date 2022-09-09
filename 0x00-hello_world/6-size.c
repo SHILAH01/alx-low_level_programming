c76bd8c76aac9881534b6b88a88db947520d7e4a
Ezeoru Shilah <ezeoruchiamaka@gmail.com>
2022-09-09 05:15:17 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+/**
+ * main- prints size of many var types
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("size of char: %d byte(s)\n", sizeof(a));
+	printf("size of int: %d byte(s)\n", sizeof(b));
+	printf("size of long int: %d byte(s)\n", sizeof(c));
+	printf("size of long long int: %d byte(s)\n", sizeof(d));
+	printf("size of float: %d byte(s)\n", sizeof(e));
+	return (0);
+}
