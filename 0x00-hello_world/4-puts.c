af9e8252304e294dcb78c1fdb47774a1e480a5d2
Ezeoru Shilah <ezeoruchiamaka@gmail.com>
2022-09-09 03:19:28 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - printing
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+ {
+ put("\" Programming is like building a multilingual puzzle");
+ return(0);
+ } 
