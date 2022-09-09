1c349433dba193697f806c64d4e1712780fdc8e2
Ezeoru Shilah <ezeoruchiamaka@gmail.com>
2022-09-09 04:23:54 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main - prints the string in the put function
+* main- prints the string in put function
 * description: using the main function
 * this program prints 'Programming is like building a multilingual puzzle
 * return:0
